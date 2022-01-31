cca49f810d0dab0a2b1946a2f5d004002bcac246
THANOS <93828850+SURTURBOT@users.noreply.github.com>
2022-01-31 12:51:05 +0530

    Update arts.py

--- userbot/plugins/arts.py
+++ userbot/plugins/arts.py
@@ -6,7 +6,7 @@ from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
 
 from . import *
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "LEGEND User"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "THANOSBOT User"
 
 USERID = bot.uid
 
