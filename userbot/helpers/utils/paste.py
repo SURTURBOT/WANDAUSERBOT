54142202d6aa0d92c725e4ee039488d5617d158a
THANOS <93828850+SURTURBOT@users.noreply.github.com>
2022-01-31 09:27:40 +0530

    Update paste.py

--- userbot/helpers/utils/paste.py
+++ userbot/helpers/utils/paste.py
@@ -5,7 +5,7 @@ import requests
 from userbot.Config import Config
 from userbot.helpers.logger import logging
 
-LOGS = logging.getLogger("LegendBot")
+LOGS = logging.getLogger("THANOSBOT")
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36",
