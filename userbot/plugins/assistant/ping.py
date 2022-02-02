e2ed757d13f64197a861277c5a178bf5b8ecfd4c
THANOS <93828850+SURTURBOT@users.noreply.github.com>
2022-02-02 19:49:33 +0530

    Update ping.py

--- userbot/plugins/assistant/ping.py
+++ userbot/plugins/assistant/ping.py
@@ -7,15 +7,15 @@ from userbot.Config import Config
 from userbot.plugins import *
 
 THANOSBOT_IMG = os.environ.get(
-    "BOT_PING_PIC", "https://telegra.ph/file/a9f6a3c160977352dd595.jpg"
+    "BOT_PING_PIC", "https://telegra.ph/file/c8fe5de96a7968636edc4.mp4"
 )
 ms = 4
 ALIVE = Config.ALIVE_NAME
 
-THANOS = f"**꧁•⊹٭Ping٭⊹•꧂**\n\n   ⚜ {ms}\n   ⚜ ❝𝐌𝐲 𝐌𝐚𝐬𝐭𝐞𝐫❞ ~『{ALIVE}』"
+THANOS = f"**꧁•⊹٭Ping٭⊹•꧂**\n\n   💞 {ms}\n   💞 ❝𝐌𝐲 𝐌𝐚𝐬𝐭𝐞𝐫❞ ~『{ALIVE}』"
 
 
 @tgbot.on(events.NewMessage(pattern="^/ping"))
 async def _(event):
-    GOOD = [[Button.url("⚜ Lêɠêɳ̃dẞø† ⚜", "https://t.me/LegendBot_Pros")]]
+    GOOD = [[Button.url("💔THANOS-PRO💔", "https://t.me/THANOSBOT_CHATS")]]
     await tgbot.send_file(event.chat_id, THANOSBOT_IMG, caption=THANOS, buttons=GOOD)
