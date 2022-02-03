4736de0ef0f6384f7429802b0ba3a08f4e0d307f
THANOS <93828850+SURTURBOT@users.noreply.github.com>
2022-02-03 09:15:47 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -105,7 +105,7 @@ async def killer():
             await asyncio.sleep(1)
             await bot.send_message("@BotFather", botname)
             await asyncio.sleep(1)
-            await bot.send_file("@BotFather", "https://telegra.ph/file/bdf457aee34ed8791c150.jpg)
+            await bot.send_file("@BotFather", "userbot/resources/pics/main.jpg)
             await asyncio.sleep(2)
         except Exception as e:
             print(e)
