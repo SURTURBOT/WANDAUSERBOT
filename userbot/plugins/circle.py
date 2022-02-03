d84861aacd6196a7190f0418b06b29e8e7162504
THANOS <93828850+SURTURBOT@users.noreply.github.com>
2022-02-03 17:02:07 +0530

    Update circle.py

--- userbot/plugins/circle.py
+++ userbot/plugins/circle.py
@@ -22,7 +22,7 @@ async def _(event):
     if reply_message.sender.bot:
         await edit_or_reply(event, "Reply to actual users message.")
         return
-    legend = await edit_or_reply(event, "Trying to convert...")
+    THANOSBOT = await edit_or_reply(event, "Trying to convert...")
     async with event.client.conversation(chat) as conv:
         try:
             response = conv.wait_event(
@@ -31,14 +31,14 @@ async def _(event):
             await event.client.send_message(chat, reply_message)
             response = await response
         except YouBlockedUserError:
-            await legend.edit("```Please unblock @TelescopyBot and try again```")
+            await THANOSBOT.edit("```Please unblock @TelescopyBot and try again```")
             return
         if response.text.startswith("Forward"):
-            await legend.edit(
+            await THANOSBOT.edit(
                 "```can you kindly disable your forward privacy settings for good?```"
             )
         else:
-            await legend.delete()
+            await THANOSBOT.delete()
             await event.client.send_file(
                 event.chat_id,
                 response.message.media,
