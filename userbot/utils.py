8dc864ccbf69026b1c1a92e9d02d98669831dd34
THANOS <93828850+SURTURBOT@users.noreply.github.com>
2022-02-04 13:52:44 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -74,6 +74,36 @@ def load_module(shortname):
         # support for paperplaneextended
         sys.modules["userbot.events"] = userbot.utils
         spec.loader.exec_module(mod)
+      
+        path = Path(f"userbot/plugins/{shortname}.py")
+        name = "userbot.plugins.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        mod.bot = Legend
+        mod.borg = bot
+        mod.PRO = PRO
+        mod.tbot = PRO
+        mod.Legend = Legend
+        mod.tgbot = bot.tgbot
+        mod.Var = Var
+        mod.command = command
+        mod.logger = logging.getLogger(shortname)
+        # support for uniborg
+        sys.modules["uniborg.util"] = userbot.utils
+        mod.Config = Config
+        mod.edit_or_reply = edit_or_reply
+        mod.delete_LEGEND = delete_LEGEND
+        mod.eod = delete_LEGEND
+        mod.admin_cmd = admin_cmd
+        mod.legend_cmd = admin_cmd
+        mod.sudo_cmd = sudo_cmd
+        # support for PRO-LEGENDBOT originals
+        sys.modules["LEGENDBOT.utils"] = userbot.utils
+        sys.modules["LEGENDBOT"] = userbot
+        # support for paperplaneextended
+        sys.modules["userbot.events"] = userbot.utils
+        spec.loader.exec_module(mod)
+
         # for imports
         sys.modules["userbot.plugins." + shortname] = mod
         LOGS.info("🔥⚡ＴＨΛＮ♢Ｓ－ＰＲ♢⚡🔥 ~ " + shortname)
