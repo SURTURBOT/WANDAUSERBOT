55c13cc6d09242e58035ac071e1fc0d0f165e8af
【★༒✞︎©༒ʟɛɢɛռɖ✘-թπο】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-02-06 06:33:26 -0800

    Update animations3.py

--- userbot/plugins/Xtra_Plugin/animations3.py
+++ userbot/plugins/Xtra_Plugin/animations3.py
@@ -4,7 +4,7 @@ from userbot import ALIVE_NAME
 from userbot.cmdhelp import CmdHelp
 from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "LEGEND user"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "THAONS user"
 
 
 @bot.on(admin_cmd(outgoing=True, pattern="kiler( (.*)|$)"))
