c9e3636f67161345e953a783e8a402a223fda3cc
THANOS <93828850+SURTURBOT@users.noreply.github.com>
2022-02-21 14:42:51 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -20,9 +20,9 @@ from userbot.helpers.ffunctions.utils import get_readable_time
 from . import *
 
 DEFAULTUSER = alive_name = Config.ALIVE_NAME
-thanos_row = Config.BUTTONS_IN_HELP
-thanos_emoji1 = Config.HELP_EMOJI1 or "✥"
-thanos_emoji2 = Config.HELP_EMOJI2 or "✥"
+THANOSBOT_row = Config.BUTTONS_IN_HELP
+THANOSBOT_emoji1 = Config.HELP_EMOJI1 or "✥"
+THANOSBOT_emoji2 = Config.HELP_EMOJI2 or "✥"
 mssge = cstm_pmp = (
     Config.PM_MSG
     or "I am Assistant Of My Owner\nI am Here To Protect My Owner From Scanner"
