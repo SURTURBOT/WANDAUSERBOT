550f84c505766095208d2563cdbe582a29782e22
THANOS <93828850+SURTURBOT@users.noreply.github.com>
2022-02-21 14:17:42 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -1,98 +1,164 @@
-from userbot.Config import Config
-import asyncio
-
-import requests
 from telethon import functions
+from telethon.errors import ChatSendInlineForbiddenError as noin
+from telethon.errors.rpcerrorlist import BotInlineDisabledError as noinline
+from telethon.errors.rpcerrorlist import BotMethodInvalidError as dedbot
+from telethon.errors.rpcerrorlist import YouBlockedUserError
 
-from userbot import ALIVE_NAME, CMD_LIST, SUDO_LIST
-from THANOSBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
+from userbot import CMD_LIST, bot
+from userbot.Config import Config
+from userbot.utils import admin_cmd, sudo_cmd
+
+from . import *
+
+mybot = Config.BOT_USERNAME
+if mybot.startswith("@"):
+    botname = mybot
+else:
+    botname = f"@{mybot}"
+
+msg = f"""
+**💞 LEGENDARY AF THANOS-PRO💞**
+
+  •        [💞 𝚁𝚎𝚙𝚘 💞](https://github.com/THANOSUSER/THANOS-PRO)
+
+  •  ©️ {THANOSBOT_channel} ™
+"""
+
+
+@bot.on(admin_cmd(pattern="repo$"))
+@bot.on(sudo_cmd(pattern="repo$", allow_sudo=True))
+async def repo(event):
+    try:
+        THANOSBOT = await bot.inline_query(botname, "repo")
+        await THANOSBOT[0].click(event.chat_id)
+        if event.sender_id == THANOSCEO:
+            await event.delete()
+    except (noin, dedbot):
+        await eor(event, msg)
 
 
 @bot.on(admin_cmd(pattern="help ?(.*)", outgoing=True))
+@bot.on(sudo_cmd(pattern="help ?(.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    tgbotusername = botname
+    chat = "@Botfather"
+    if tgbotusername is not None:
+        try:
+            results = await event.client.inline_query(tgbotusername, "THANOSBOT_help")
+            await results[0].click(
+                event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
+            )
+            await event.delete()
+        except noinline:
+            THANOSBOT = await eor(
+                event,
+                "**Inline Mode is disabled.** \n__Turning it on, please wait for a minute...__",
+            )
+            async with bot.conversation(chat) as conv:
+                try:
+                    first = await conv.send_message("/setinline")
+                    second = await conv.get_response()
+                    third = await conv.send_message(tgbotusername)
+                    fourth = await conv.get_response()
+                    fifth = await conv.send_message(perf)
+                    sixth = await conv.get_response()
+                    await bot.send_read_acknowledge(conv.chat_id)
+                except YouBlockedUserError:
+                    return await THANOSBOT.edit("Unblock @Botfather first.")
+                await THANOSBOT.edit(
+                    f"**thanos Turned On the Inline Mode Successfully.** \n\nDo `{l1}op` again to get the help menu."
+                )
+            await bot.delete_messages(
+                conv.chat_id,
+                [first.id, second.id, third.id, fourth.id, fifth.id, sixth.id],
+            )
+    else:
+        await eor(
+            event,
+            "**⚠️ 𝙴𝚁𝚁𝙾𝚁 !!** \n𝙿𝚕𝚎𝚊𝚜𝚎 𝚁𝚎-𝙲𝚑𝚎𝚌𝚔 BOT_TOKEN & BOT_USERNAME on Heroku.",
+        )
+
+
+@bot.on(admin_cmd(pattern="priya ?(.*)", outgoing=True))
 async def yardim(event):
     if event.fwd_from:
         return
-    tgbotusername = Config.BOT_USERNAME
+    tgbotusername = botname
     input_str = event.pattern_match.group(1)
     if tgbotusername is not None or THANOSBOT_input == "text":
-        results = await event.client.inline_query(tgbotusername, "@thanosBot_chat")
+        results = await event.client.inline_query(tgbotusername, "THANOSBOT_help")
         await results[0].click(
             event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
         )
         await event.delete()
     else:
-        await edit_or_reply(event, ["NO_BOT"])
-    
+        await eor(event, "**Check Bot Token And Bot Username In Reveal Var*")
+
         if input_str in CMD_LIST:
-          string = "Commands found in {}:\n".format(input_str)
-          for i in CMD_LIST[input_str]:
-              string += "  " + i
-              string += "\n"
-          await event.edit(string)
+            string = "Commands found in {}:\n".format(input_str)
+            for i in CMD_LIST[input_str]:
+                string += "  " + i
+                string += "\n"
+            await event.edit(string)
         else:
-          await event.edit(input_str + " is not a valid plugin!")
+            await event.edit(input_str + " is not a valid plugin!")
 
 
-@bot.on(sudo_cmd(allow_sudo=True, pattern="help ?(.*)"))
-async def info(event):
+@bot.on(admin_cmd(pattern="ihelp(?: |$)(.*)", outgoing=True))
+@bot.on(sudo_cmd(pattern="ihelp(?: |$)(.*)", allow_sudo=True))
+async def THANOSBOT(event):
     if event.fwd_from:
         return
-    input_str = event.pattern_match.group(1)
-    if input_str == "text":
-        string = (
-            "Total {count} commands found in {plugincount} sudo plugins of ThanosBot\n\n"
-        )
-        THANOSBOTcount = 0
-        plugincount = 0
-        for i in sorted(SUDO_LIST):
-            plugincount += 1
-            string += f"{plugincount}) Commands found in Plugin " + i + " are \n"
-            for iter_list in SUDO_LIST[i]:
-                string += "    " + str(iter_list)
-                string += "\n"
-                THANOSBOTcount += 1
-            string += "\n"
-        if len(string) > 4095:
-            data = string.format(count=THANOSBOTcount, plugincount=plugincount)
-            key = (
-                requests.post(
-                    "https://nekobin.com/api/documents", json={"content": data}
-                )
-                .json()
-                .get("result")
-                .get("key")
-            )
-            url = f"https://nekobin.com/{key}"
-            reply_text = f"All commands of the THANOSBOT are [here]({url})"
-            await event.reply(reply_text, link_preview=False)
-            return
-        await event.reply(
-            string.format(count=THANOSBOTcount, plugincount=plugincount), link_preview=False
-        )
-        return
-    if input_str:
-        if input_str in SUDO_LIST:
-            string = "<b>{count} Commands found in plugin {input_str}:</b>\n\n"
-            THANOSBOTcount = 0
-            for i in SUDO_LIST[input_str]:
-                string += f"  •  <code>{i}</code>"
-                string += "\n"
-                THANOSBOTcount += 1
-            await event.reply(
-                string.format(count=THANOSBOTcount, input_str=input_str), parse_mode="HTML"
-            )
+    args = event.pattern_match.group(1).lower()
+    if args:
+        if args in CMD_HELP:
+            await eor(event, str(CMD_HELP[args]))
         else:
-            reply = await event.reply(input_str + " is not a valid plugin!")
-            await asyncio.sleep(3)
-            await event.delete()
-            await reply.delete()
+            await eor(event, "**⚠️Sorry !** \nPlugin 𝚗𝚊𝚖𝚎 𝚝𝚘 𝚜𝚑𝚘𝚠 𝚙𝚕𝚞𝚐𝚒𝚗 𝚒𝚗𝚏𝚘")
     else:
-        string = "<b>Please specify which plugin do you want help for !!\
-            \nNumber of plugins : </b><code>{count}</code>\
-            \n<b>Usage:</b> <code>.help plugin name</code>\n\n"
-        THANOSBOTcount = 0
-        for i in sorted(SUDO_LIST):
-            string += "≈ " + f"<code>{str(i)}</code>"
-            string += " "
-            THANOSBOTcount += 1
-        await event.reply(string.format(count=THANOSBOTcount), parse_mode="HTML")
+        string = ""
+        sayfa = [
+            sorted(list(CMD_HELP))[i : i + 5]
+            for i in range(0, len(sorted(list(CMD_HELP))), 5)
+        ]
+
+        for i in sayfa:
+            string += f"`👉`"
+            for sira, a in enumerate(i):
+                string += "`" + str(a)
+                if sira == i.index(i[-1]):
+                    string += "`"
+                else:
+                    string += "`, "
+            string += "\n"
+        await eor(
+            event,
+            "Please Specify A Module Name Of Which You Want Info" + "\n\n" + string,
+        )
+
+
+@borg.on(admin_cmd(pattern="config"))  # pylint:disable=E0602
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    result = await borg(functions.help.GetConfigRequest())  # pylint:disable=E0602
+
+    result = result.stringify()
+
+    logger.info(result)  # pylint:disable=E0602
+
+    await event.edit("тєℓєтнση  вαѕє∂ υѕєявσт ρσωєяє∂ ву **THANOS** вσт")
+
+
+CmdHelp("helper").add_command("repo", None, "To Get Repo And Repl Link").add_command(
+    "help", None, "To Get Help Menu"
+).add_command(
+    "priya", "<plugin name>", "To Get Detail About Plugin", "priya alive"
+).add_command(
+    "ihelp", "<Pluggin Name>", "To get detail about any plugin"
+).add()
