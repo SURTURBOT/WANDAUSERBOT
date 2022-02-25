3798a4bf6805c88bb8671a231ab60854bebc24dc
RISHABH ANANAD <94896142+thanosuser@users.noreply.github.com>
2022-02-25 20:00:47 +0530

    Create apersonal.py

---
+++ userbot/plugins/apersonal.py
@@ -0,0 +1,33 @@
+# this plugin maked by @thanosceo for thanos pro kang whith credits 
+#thanos pro
+import asyncio
+from userbot import *
+from THANOSBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
+from userbot.cmdhelp import CmdHelp
+from userbot import ALIVE_NAME
+#thanospro
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "THANOSBOT"
+
+
+@borg.on(admin_cmd(outgoing=True, pattern="^Pm$"))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 2
+    animation_ttl = range(0, 17)
+    await event.edit("❤")
+    animation_chars = [
+        "ＳＵＮ♢",
+        "ＰΞＲＳ♢ＮΛＬ ＭΞＳＳΛＧΞ ＫΛＲ♢",
+        f"ＰΞＲＳ♢ＮΛＬ ＭΞＳＳΛＧΞ ＫΛＲ♢",    
+    ]
+    for i in animation_ttl:  # By @thanosceo for thanos bot
+
+        await asyncio.sleep(animation_interval)
+        await event.edit(
+            animation_chars[i % 17], link_preview=True
+        ) 
+
+from userbot.cmdhelp import CmdHelp
+
+CmdHelp("PERSONAL").add_command("Pm", None, "PM FULL FORM").add()
