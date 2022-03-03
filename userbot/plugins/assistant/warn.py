112e90cd411f64cd0ada948b3b1c52aa80276982
THANOS <93828850+SURTURBOT@users.noreply.github.com>
2022-03-03 15:43:44 +0530

    Update warn.py

--- userbot/plugins/assistant/warn.py
+++ userbot/plugins/assistant/warn.py
@@ -5,7 +5,7 @@ from telethon.tl.types import ChannelParticipantsAdmins
 from userbot.utils import admin_cmd
 
 
-@borg.on(admin_cmd("/warn"))
+@tgbot.on(events.NewMessage(pattern="^/warn"))
 async def _(event):
     if event.fwd_from:
         return
@@ -28,7 +28,7 @@ from telethon.tl.types import ChannelParticipantsAdmins
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd("/warn2"))
+@tgbot.on(events.NewMessage(pattern="^/warn2"))
 async def _(event):
     if event.fwd_from:
         return
@@ -51,7 +51,7 @@ from telethon.tl.types import ChannelParticipantsAdmins
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd("/warn3"))
+@tgbot.on(events.NewMessage(pattern="^/warn3"))
 async def _(event):
     if event.fwd_from:
         return
@@ -74,7 +74,7 @@ from telethon.tl.types import ChannelParticipantsAdmins
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd("/warn0"))
+@tgbot.on(events.NewMessage(pattern="^/warn0"))
 async def _(event):
     if event.fwd_from:
         return
@@ -98,7 +98,7 @@ from uniborg.util import admin_cmd
 
 
 
-@borg.on(admin_cmd("/ocb"))
+@tgbot.on(events.NewMessage(pattern="^/ocb"))
 async def _(event):
     if event.fwd_from:
         return
@@ -114,7 +114,7 @@ async def _(event):
         await event.reply(mentions)
     await event.delete()
 
-@borg.on(admin_cmd("/fw"))
+@tgbot.on(events.NewMessage(pattern="^/fw"))
 async def _(event):
     if event.fwd_from:
         return
