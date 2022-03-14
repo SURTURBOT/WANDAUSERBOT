19534bcc8b8ae852eb87fa9f6e4d9a2ae740ee38
RISHABH ANANAD <94896142+thanosuser@users.noreply.github.com>
2022-03-14 08:24:22 +0530

    Update telegraph.py

--- userbot/plugins/telegraph.py
+++ userbot/plugins/telegraph.py
@@ -1,104 +1,27 @@
-import datetime
+# op
 import os
+import random
+import string
+from datetime import datetime
 
 from PIL import Image
-from telegraph import Telegraph, upload_file
-from telegraph.exceptions import TelegraphException
+from telegraph import Telegraph, exceptions, upload_file
+from telethon.utils import get_display_name
 
-from userbot.cmdhelp import CmdHelp
-from userbot.Config import Config
-from userbot.utils import admin_cmd, edit_or_reply, eor, sudo_cmd
+from userbot import THANOSBOT
 
-from . import THANOSBOT_mention
+from ..Config import Config
+from ..core.logger import logging
+from ..core.managers import eor
+from . import BOTLOG, BOTLOG_CHATID, mention
 
-THANOSBOT_NAME = Config.ALIVE_NAME or "THANOSBOT"
-lg_id = Config.LOGGER_ID
+LOGS = logging.getLogger(__name__)
+menu_category = "utils"
+
 
 telegraph = Telegraph()
 r = telegraph.create_account(short_name=Config.TELEGRAPH_SHORT_NAME)
 auth_url = r["auth_url"]
-
-
-@bot.on(admin_cmd(pattern=f"t(m|t) ?(.*)", outgoing=True))
-@bot.on(sudo_cmd(pattern=f"t(m|t) ?(.*)", allow_sudo=True))
-async def _(event):
-    if event.fwd_from:
-        return
-    if Config.LOGGER_ID is None:
-        await eod(event, "You need to setup `LOGGER_ID` to use telegraph...")
-        return
-    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
-        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
-    optional_title = event.pattern_match.group(2)
-    if event.reply_to_msg_id:
-        start = datetime.datetime.now()
-        r_message = await event.get_reply_message()
-        input_str = event.pattern_match.group(1)
-        if input_str == "m":
-            downloaded_file_name = await bot.download_media(
-                r_message, Config.TMP_DOWNLOAD_DIRECTORY
-            )
-            end = datetime.datetime.now()
-            ms = (end - start).seconds
-            await eor(
-                event,
-                "Downloaded to  `{}`  in  `{}`  seconds. \nMaking Telegraph Link.....".format(
-                    downloaded_file_name, ms
-                ),
-            )
-            if downloaded_file_name.endswith((".webp")):
-                resize_image(downloaded_file_name)
-            try:
-                start = datetime.datetime.now()
-                media_urls = upload_file(downloaded_file_name)
-            except TelegraphException as exc:
-                await eod(event, "ERROR: " + str(exc), 8)
-                os.remove(downloaded_file_name)
-            else:
-                end = datetime.datetime.now()
-                ms_two = (end - start).seconds
-                os.remove(downloaded_file_name)
-                await eor(
-                    event,
-                    "✓ **[File uploaded to telegraph](https://telegra.ph{})** \n✓ **Time Taken :-** `{}` secs \n✓ **By :- {}** \n✓  `https://telegra.ph{}`".format(
-                        media_urls[0],
-                        (ms + ms_two),
-                        legend_mention,
-                        media_urls[0],
-                    ),
-                    link_preview=True,
-                )
-        elif input_str == "t":
-            user_object = await borg.get_entity(r_message.sender_id)
-            title_of_page = user_object.first_name  # + " " + user_object.last_name
-            # apparently, all Users do not have last_name field
-            if optional_title:
-                title_of_page = optional_title
-            page_content = r_message.message
-            if r_message.media:
-                if page_content != "":
-                    title_of_page = page_content
-                downloaded_file_name = await borg.download_media(
-                    r_message, Config.TMP_DOWNLOAD_DIRECTORY
-                )
-                m_list = None
-                with open(downloaded_file_name, "rb") as fd:
-                    m_list = fd.readlines()
-                for m in m_list:
-                    page_content += m.decode("UTF-8") + "\n"
-                os.remove(downloaded_file_name)
-            page_content = page_content.replace("\n", "<br>")
-            response = telegraph.create_page(title_of_page, html_content=page_content)
-            end = datetime.datetime.now()
-            ms = (end - start).seconds
-            legendboy = f"https://telegra.ph/{response['path']}"
-            await edit_or_reply(
-                event,
-                f"✓ **[Pasted to telegraph]({legendboy})** \n✓ **Time Taken :-** `{ms}` secs\n✓** By :**  {legend_mention} \n✓  `{legendboy}`",
-                link_preview=True,
-            )
-    else:
-        await eod(event, "Reply to a message to get a permanent telegra.ph link.")
 
 
 def resize_image(image):
@@ -106,16 +29,104 @@ def resize_image(image):
     im.save(image, "PNG")
 
 
-CmdHelp("telegraph").add_command(
-    "tt",
-    "<reply to text message>",
-    "Uploads the replied text message to telegraph making a short telegraph link",
-).add_command(
-    "tm",
-    "<reply to media>",
-    "Uploads the replied media (sticker/ gif/ video/ image) to telegraph and gives a short telegraph link",
-).add_info(
-    "Make Telegraph Links."
-).add_warning(
-    "✅ Harmless Module."
-).add()
+@bot.on_cmd(
+    pattern="(t(ele)?g(raph)?) ?(m|t|media|text)(?:\s|$)([\s\S]*)",
+    command=("telegraph", menu_category),
+    info={
+        "header": "To get telegraph link.",
+        "description": "Reply to text message to paste that text on telegraph you can also pass input along with command             So that to customize title of that telegraph and reply to media file to get sharable link of that media(atmost 5mb is supported)",
+        "options": {
+            "m or media": "To get telegraph link of replied sticker/image/video/gif.",
+            "t or text": "To get telegraph link of replied text you can use custom title.",
+        },
+        "usage": [
+            "{tr}tgm",
+            "{tr}tgt <title(optional)>",
+            "{tr}telegraph media",
+            "{tr}telegraph text <title(optional)>",
+        ],
+    },
+)  # sourcery no-metrics
+async def _(event):
+    "To get telegraph link."
+    THANOSBOTevent = await eor(event, "`processing........`")
+    if BOTLOG:
+        await event.client.send_message(
+            BOTLOG_CHATID,
+            f"Created New Telegraph account {auth_url} for the current session. 
+**Do not give this url to anyone, even if they say they are from Telegram!**",
+        )
+    optional_title = event.pattern_match.group(5)
+    if not event.reply_to_msg_id:
+        return await THANOSBOTevent.edit(
+            "`Reply to a message to get a permanent telegra.ph link.`",
+        )
+
+    start = datetime.now()
+    r_message = await event.get_reply_message()
+    input_str = (event.pattern_match.group(4)).strip()
+    if input_str in ["media", "m"]:
+        downloaded_file_name = await event.client.download_media(
+            r_message, Config.TEMP_DIR
+        )
+        await THANOSBOTevent.edit(f"`Downloaded to {downloaded_file_name}`")
+        if downloaded_file_name.endswith((".webp")):
+            resize_image(downloaded_file_name)
+        try:
+            media_urls = upload_file(downloaded_file_name)
+        except exceptions.TelegraphException as exc:
+            await THANOSBOTevent.edit(f"**Error : **
+`{exc}`")
+            os.remove(downloaded_file_name)
+        else:
+            end = datetime.now()
+            ms = (end - start).seconds
+            os.remove(downloaded_file_name)
+            await THANOSBOTevent.edit(
+                f"**✓ Uploaded to :-**[telegraph](https://telegra.ph{media_urls[0]})                 
+**✓ Uploaded in {ms} seconds.**                 
+**✓ Uploaded by :-** {mention}                 
+**✓ Telegraph :** `https://telegra.ph{media_urls[0]}`",
+                link_preview=True,
+            )
+    elif input_str in ["text", "t"]:
+        user_object = await event.client.get_entity(r_message.sender_id)
+        title_of_page = get_display_name(user_object)
+        # apparently, all Users do not have last_name field
+        if optional_title:
+            title_of_page = optional_title
+        page_content = r_message.message
+        if r_message.media:
+            if page_content != "":
+                title_of_page = page_content
+            downloaded_file_name = await event.client.download_media(
+                r_message, Config.TEMP_DIR
+            )
+            m_list = None
+            with open(downloaded_file_name, "rb") as fd:
+                m_list = fd.readlines()
+            for m in m_list:
+                page_content += m.decode("UTF-8") + "
+"
+            os.remove(downloaded_file_name)
+        page_content = page_content.replace("
+", "<br>")
+        try:
+            response = telegraph.create_page(title_of_page, html_content=page_content)
+        except Exception as e:
+            LOGS.info(e)
+            title_of_page = "".join(
+                random.choice(list(string.ascii_lowercase + string.ascii_uppercase))
+                for _ in range(16)
+            )
+            response = telegraph.create_page(title_of_page, html_content=page_content)
+        end = datetime.now()
+        ms = (end - start).seconds
+        THANOSBOT = f"https://telegra.ph/{response['path']}"
+        await THANOSBOTevent.edit(
+            f"**✓ Uploaded to :-** [telegraph]({THANOSBOT})                 
+**✓ Uploaded in {ms} seconds.**                 
+**✓ Uploaded by :-** {mention}                 
+**✓ Telegraph :-** `{THANOSBOT}`",
+            link_preview=True,
+        )
