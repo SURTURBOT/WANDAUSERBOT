0660494c7838fe3349556547168fc19375a04c4e
RISHABH ANANAD <94896142+thanosuser@users.noreply.github.com>
2022-03-20 14:22:38 +0530

    Create THANOSSHYRI.py

---
+++ userbot/plugins/THANOSSHYRI.py
@@ -0,0 +1,13 @@
+'''This plugin maked by rishabh kang with credits'''
+import asyncioimport randomfrom asyncio 
+import sleep from userbot 
+import ALIVE_NAMEfrom userbot.cmdhelp 
+import CmdHelpfrom userbot.utils 
+import admin_cmd, edit_or_reply, sudo_cmd DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "LEGEND User" 
+thanos = bot.uid
+
+
+
+
+@bot.on(admin_cmd(pattern=r"snow$", outgoing=True))
+if x == 104: await event.edit("Dil Se Chahne Ki Mujhe Saza Dete Ho ")
